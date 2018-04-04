e862f7fffd45f8c8c6c7c037e10dea93fb90095b
anurag <arun.etech2011@gmail.com>
2018-04-04 15:32:06 +0530

    added add toDo form working

--- src/Displaylist.jsx
+++ src/Displaylist.jsx
@@ -7,16 +7,21 @@ export default class Displaylist extends React.Component {
   }
   render(){
     return (<ul style={{'listStyle': 'none', 'paddingLeft': '0px'}}>
-      {this.props.todoList.map((item,i) => 
-        <li style={{'display': 'block','width':'700px', 'border': '1px solid rgba(0,0,0,.1)', 'borderRadius': '2px', 'padding': '3px 10px' }} key={i}>
+      {this.props.toDoList.map((item,i) => 
+        <li style={{'display': 'block', 'width':'700px', 'border': '1px solid rgba(0,0,0,.1)', 'borderRadius': '2px', 'padding': '3px 10px' }} key={i}>
           <input
+            onChange={() => this.props.toggle(item.id, 'check')}
             checked={item.checked}
             type="checkbox"
             style={{ fontSize:'x-large'}}/>
           <span style={{'display': 'inlineBlock', 'padding': '0px 10px' }}>{item.name}</span>
           {
             item.checked ? 
-              <a style={{'display': 'inlineBlock', 'float': 'right', 'padding': '0px 10px', 'background': 'grey', 'borderRadius': '10px', 'fontSize': '10px', 'marginTop': '4px', 'color': '#fff', 'cursor': 'pointer' }}>completed</a>
+              <a
+                style={{'display': 'inlineBlock', 'float': 'right', 'padding': '0px 10px', 'background': 'darkgrey', 'borderRadius': '10px', 'fontSize': '10px', 'marginTop': '4px', 'color': '#fff', 'cursor': 'pointer' }}
+                onClick={() => this.props.toggle(item.id, 'delete')}>
+                completed
+              </a>
               :
             null
           }
--- src/app.jsx
+++ src/app.jsx
@@ -6,12 +6,27 @@ export default class App extends React.Component {
   constructor(){
     super();
     this.state={
-      todoName: '',
-      todoList: [
+      toDoName: '',
+      toDoList: [
         {id: 0, name: 'vikas', checked: true}, 
         {id: 1, name: 'nitesh', checked: false}
       ]                                           // hard coded json data
     };
+  }
+  toggle(itemToBeDeleted, Case){
+    console.log(this.state.toDoList, itemToBeDeleted, Case);
+    if(Case === 'check'){
+      
+    } else if(Case === 'delete'){
+
+    }
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    const { toDoName, toDoList } = this.state;
+    const newToDoList = toDoList;
+    newToDoList.push({id: toDoList.length, name: toDoName, checked: false});
+    this.setState({toDoName:'', toDoList: newToDoList});
   }
   render() {
     return (
@@ -19,7 +34,23 @@ export default class App extends React.Component {
         <div className='row'>
           <h1 style={{'padding': '1em 0 0 .5em'}}>Todo App</h1>
         </div>
-        <Displaylist todoList={this.state.todoList}/>
+        <Displaylist
+          toggle={this.toggle.bind(this)}
+          toDoList={this.state.toDoList}
+        />
+        <div className='row'>
+          <h5 className='col-xs-12' style={{'padding': '1em 0 0 .5em'}}>Todo</h5>
+          <form className='col-xs-12' onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              style={{'display': 'block', 'outline': 'none', 'border': '1px solid rgba(0,0,0,.1)', 'borderRadius': '2px', 'width': '700px', 'padding': '3px 5px' }}
+              onChange={(e) => this.setState({toDoName: e.target.value})}
+              value={this.state.toDoName}
+              />
+            <button
+              style={{'display': 'block', 'outline': 'none', 'border': '1px solid rgba(0,0,0,.1)', 'borderRadius': '4px', 'marginTop': '10px', 'background': '#fff'}}
+            > submit </button>
+          </form>
+        </div>
       </div>
     )
   }
