6c20bfb699ecdd8d005f8a35d30bf603aff5c8a3
reactexcel <arun.etech2011@gmail.com>
2018-04-30 16:18:24 +0530

    added immutability-helper for updating reducer

--- src/redux/home/reducer/index.js
+++ src/redux/home/reducer/index.js
@@ -1,9 +1,10 @@
 import { handleActions } from 'redux-actions';
+import update from 'immutability-helper';
 
 export const initialState = {
   list: ''
 };
 
 export const home = handleActions({
-  CHANGE_NAME: (state, action) => ({ list: action.payload })
+  CHANGE_NAME: (state, action) => update(state, { list: { $set: action.payload } })
 }, initialState);
